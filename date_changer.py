6e69c2eb82824084b459b4febd3822ea20c0f0b4
BogdNV <nikolaevbv92@gmail.com>
2024-12-06 22:45:42 +0300

    remove datetime

--- date_changer.py
+++ date_changer.py
@@ -1,7 +1,7 @@
 created_date = "06-12-2024"
 issue_date = "13-12-2024"
 
-temp_created_date = created_date[-5:]
+temp_created_date = created_date[:-5]
 temp_issue_date = issue_date[:-5]
 
 print(temp_created_date)
