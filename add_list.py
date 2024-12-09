04078e9812060da2e20a5d34c2837d0febc6ea6f
BogdNV <nikolaevbv92@gmail.com>
2024-12-09 20:40:45 +0300

    add 'add_list.py'

---
+++ add_list.py
@@ -0,0 +1,9 @@
+title1 = input("Добавте заметку №1").strip()
+title2 = input("Добавте заметку №2").strip()
+title3 = input("Добавте заметку №3").strip()
+
+titles = [title1, title2, title3]
+
+print(f"Заметка №1 {title1}")
+print(f"Заметка №1 {title2}")
+print(f"Заметка №1 {title3}")
