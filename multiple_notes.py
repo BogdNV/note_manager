545995ce6e5fe7a3c30780bac4f57957c8601243
BogdNV <nikolaevbv92@gmail.com>
2024-12-17 19:12:03 +0300

    update "multiple_notes.py" add check_note, get_name_title

--- multiple_notes.py
+++ multiple_notes.py
@@ -33,7 +33,7 @@ def display_notes(notes):
     if notes:
         print("\nСписок заметок:")
         for i, note in enumerate(notes):
-            print(f"{i+1}. Имя: {note['user']}")
+            print(f"{i+1}. Имя: {note['name']}")
             print((" "*len(f"{i+1}. ")) + f"Заголовок: {note["title"]}")
             print((" "*len(f"{i+1}. ")) + f"Описание: {note["content"]}")
             print((" "*len(f"{i+1}. ")) + f"Статус: {note["status"]}")
@@ -45,7 +45,7 @@ def display_notes(notes):
 
 def check_note(dict_list, name, title):
     for d in dict_list:
-        if d.get("name") == name and d.get("content") == title:
+        if d.get("name", None) == name and d.get("title", None) == title:
             return True
     return False
 
@@ -68,19 +68,21 @@ def get_note():
 
     note = {}
     name, title = get_name_title()
-    if not check_note(notes, name, title):
+    if check_note(notes, name, title):
         print("Такая заметка уже существует")
         return
 
     content = input("Введите описание заголовка: ").strip()
     status = get_status()
     issue_date = get_date("%d-%m-%Y")
+
     note["name"] = name
     note["title"] = title
     note["content"] = content
     note["status"] = status
     note["created_date"] = date_now
     note["issue_date"] = issue_date
+
     return note
 
 
