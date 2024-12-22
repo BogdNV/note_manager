fab973100979464f05b8cfca626c1b8ab78e71fd
BogdNV <nikolaevbv92@gmail.com>
2024-12-22 13:52:48 +0300

    add "display_notes_function.py"

--- multiple_notes.py
+++ multiple_notes.py
@@ -38,14 +38,17 @@ def display_notes(notes):
         print("-"*20)
         for i, note in enumerate(notes):
             print(f"{i+1}. Имя: {note.get('username', "")}")
-            print((" "*len(f"{i+1}. ")) + f"Заголовок: {note.get("title", "")}")
-            print((" "*len(f"{i+1}. ")) + f"Описание: {note.get("content", "")}")
-            print((" "*len(f"{i+1}. ")) + f"Статус: {note.get("status","")}")
-            print((" "*len(f"{i+1}. ")) + f"Дата создания: {note.get("created_date", date_now).strftime("%d-%m-%Y")}")
-            print((" "*len(f"{i+1}. ")) + f"Дедлайн: {note.get("issue_date", date_now).strftime("%d-%m-%Y")}")
+            ind = " "*len(f"{i+1}. ")
+            print(ind + f"Заголовок: {note.get("title", "")}")
+            print(ind + f"Описание: {note.get("content", "")}")
+            print(ind + f"Статус: {note.get("status","")}")
+            print(ind + f"Дата создания: {note.get("created_date", date_now).strftime("%d-%m-%Y")}")
+            print(ind + f"Дедлайн: {note.get("issue_date", date_now).strftime("%d-%m-%Y")}")
             print("-"*20)
     else:
+        print("-" * 20)
         print("Список заметок пуст.")
+        print("-" * 20)
 
 #проверяет существует ли заметка в списке
 def check_note(dict_list, name, title):
