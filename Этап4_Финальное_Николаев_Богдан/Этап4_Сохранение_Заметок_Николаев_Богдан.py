94c56b25a3393cd5257297172fd4157ba34f0ab9
BogdNV <nikolaevbv92@gmail.com>
2025-01-10 23:25:41 +0300

    release save_notes_to_file

--- Этап4_Финальное_Николаев_Богдан/Этап4_Сохранение_Заметок_Николаев_Богдан.py
+++ Этап4_Финальное_Николаев_Богдан/Этап4_Сохранение_Заметок_Николаев_Богдан.py
@@ -8,18 +8,18 @@ def save_notes_to_file(notes, filename):
     try:
         with open(filename, 'w', encoding='utf-8') as file:
             for note in notes:
-                file.write(f"- Имя пользователя: {note.get("username")}\n")
-                file.write(f"  Заголовок: {note.get("title")}\n")
-                file.write(f"  Описание: {note.get("content")}\n")
-                file.write(f"  Статус: {note.get("status")}\n")
+                file.write(f"- username: {note.get("username")}\n")
+                file.write(f"  title: {note.get("title")}\n")
+                file.write(f"  content: {note.get("content")}\n")
+                file.write(f"  status: {note.get("status")}\n")
                 created_date = note.get("created_date")
                 issue_date = note.get("issue_date")
                 if isinstance(created_date, datetime.date):
                     created_date = created_date.strftime("%d-%m-%Y")
                 if isinstance(issue_date, datetime.date):
                     issue_date = issue_date.strftime("%d-%m-%Y")
-                file.write(f"  Дата создания: {created_date}\n")
-                file.write(f"  Дедлайн: {issue_date}\n")
+                file.write(f"  created_date: {created_date}\n")
+                file.write(f"  issue_date: {issue_date}\n")
                 file.write("\n")
     except Exception as e:
         print(f"Что-то пошло не так\n{e}")
