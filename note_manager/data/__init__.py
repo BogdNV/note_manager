77de259eac8516b56c00501eb4d743c8e1f281d2
BogdNV <nikolaevbv92@gmail.com>
2025-01-18 17:57:36 +0300

    init from data

--- note_manager/data/__init__.py
+++ note_manager/data/__init__.py
@@ -1,1 +1,2 @@
-from .file_handling import save_notes_to_file, load_notes_from_file, append_notes_to_file, save_notes_json
+from .file_handling import (save_notes_to_file, load_notes_from_file,
+                            append_notes_to_file, save_notes_json, load_notes_json)
