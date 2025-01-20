db4489285c852a1d20938009a416db7e36b75fbe
BogdNV <nikolaevbv92@gmail.com>
2025-01-20 14:12:01 +0300

    add class NoteManager

--- note_manager/main.py
+++ note_manager/main.py
@@ -7,10 +7,23 @@ from utils import _statuses, validate_name_title, validate_date, validate_note
 from interface import create_note, search_notes
 from tests import generate_notes
 
-
-notes = []
 date_now = dt.now().date()
 
+class NoteManager():
+    def __init__(self):
+        self.notes = []
+
+    def add_note(self, note):
+        if not validate_note(self.notes, note):
+            raise ValueError("Заметка с таким пользователем уже существует")
+        if isinstance(note, dict):
+            self.notes.append(note)
+        elif isinstance(note, list):
+            self.notes.extend(note)
+
+
+    def get_notes(self, keyword=None, status=None):
+        return search_notes(self.notes, keyword=keyword, status=status)
 
 
 class App(tk.Tk):
@@ -31,7 +44,7 @@ class App(tk.Tk):
 
         self.show_frame(StartFrame)
 
-    def show_frame(self, frame_class):
+    def show_frame(self, frame_class, *args, **kwargs):
         """Показать указанное окно"""
         if self.current_frame:
             # Удаляем текущее окно
@@ -39,7 +52,7 @@ class App(tk.Tk):
         # Сохраняем класс текущего окна как предыдущий
         self.previous_frame = self.current_frame.__class__ if self.current_frame else None
         # Создаем новое окно
-        self.current_frame = frame_class(self)
+        self.current_frame = frame_class(self, *args, **kwargs)
         self.current_frame.pack(expand=True, fill="both")
 
 
@@ -51,7 +64,7 @@ class App(tk.Tk):
 
         
 class BaseFrame(tk.Frame):
-    _notes = []
+    _nm = NoteManager()
     def __init__(self, master):
         super().__init__(master)
         if self.__class__.__name__ == "StartFrame":
@@ -69,7 +82,14 @@ class BaseFrame(tk.Frame):
             self.master.destroy()
 
     def create_tree(self, list_notes):
-        columns = ("name", "title", "content", "status", "created_date", "issue_date")
+        columns = {
+            "name":"Имя",
+            "title":"Заголовок",
+            "content":"Описание",
+            "status":"Статус",
+            "created_date":"Дата создания",
+            "issue_date":"Дата дедлайна"
+        }
 
         if self.tree:
             self.tree.destroy()
@@ -78,23 +98,13 @@ class BaseFrame(tk.Frame):
 
         self.tree = ttk.Treeview(
             self,
-            columns=columns,
+            columns=list(columns.keys()),
             show="headings",
             height=15
         )
         self.tree.pack(side=tk.LEFT, padx=6)
 
-        self.tree.heading("name", text="Имя", anchor=tk.W, command=lambda: self.sort_column("name"))
-        self.tree.heading("title", text="Заголовок", anchor=tk.W, command=lambda: self.sort_column("title"))
-        self.tree.heading("content", text="Описание", anchor=tk.W, command=lambda: self.sort_column("content"))
-        self.tree.heading("status", text="Статус", anchor=tk.W, command=lambda: self.sort_column("status"))
-        self.tree.heading("created_date", text="Дата создания", anchor=tk.W,
-                          command=lambda: self.sort_column("created_date"))
-        self.tree.heading("issue_date", text="Дата дедлайна", anchor=tk.W,
-                          command=lambda: self.sort_column("issue_date"))
-
-        for i, c in enumerate(columns):
-            self.tree.column(f"#{i}", width=100)
+        self.configure_tree_columns(columns)
 
         for note in list_notes:
             if isinstance(note["created_date"], datetime.date):
@@ -107,6 +117,11 @@ class BaseFrame(tk.Frame):
         self.tree.configure(yscroll=self.scrollbar.set)
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
+    def configure_tree_columns(self, columns):
+        for col in columns:
+            self.tree.heading(col, text=columns[col], anchor=tk.W, command=lambda c=col: self.sort_column(c))
+            self.tree.column(col, width=110)
+
     def sort_column(self, col, reverse=False):
         data = [(self.tree.set(child, col), child) for child in self.tree.get_children()]
 
@@ -117,42 +132,29 @@ class BaseFrame(tk.Frame):
 
         self.tree.heading(col, command=lambda : self.sort_column(col, not reverse))
 
+
+    def create_buttom(self, text, command, width=30, pady=6, side="top"):
+        but = ttk.Button(self, text=text, command=command, width=width)
+        but.pack(pady=pady, side=side)
+        return but
+
 class StartFrame(BaseFrame):
     def __init__(self, master):
         super().__init__(master)
-
-        # Кнопка "Создать файл"
-        create_note_btn = ttk.Button(self, text="Создать новую заметку", width=30,
-                                    command=lambda: master.show_frame(CreateNoteFrame))
-        create_note_btn.pack(pady=6)
-
-
-        display_notes_btn = ttk.Button(self, text="Показать текущие заметки", width=30,
-                                    command=lambda: master.show_frame(DisplayNotesFrame))
-        display_notes_btn.pack(pady=6)
-
-
-        update_note_btn = ttk.Button(self, text="Обновить заметку", width=30,
-                                     command=lambda: master.show_frame(UpdateNoteFrame))
-        update_note_btn.pack(pady=6)
-
-
-        delete_note_btn = ttk.Button(self, text="Удалить заметку", width=30,
-                                     command=lambda: master.show_frame(DeleteNoteFrame))
-        delete_note_btn.pack(pady=6)
-
-
-        search_notes_btn = ttk.Button(self, text="Найти заметку", width=30,
-                                      command=lambda: master.show_frame(SearchNoteFrame))
-        search_notes_btn.pack(pady=6)
-
-        create_test_notes = ttk.Button(self, text="Создать тестовые заметки", width=30,
-                                       command=self.create_notes)
-        create_test_notes.pack(side="bottom")
+        create_note_btn = self.create_buttom("Создать новую заметку", lambda: master.show_frame(CreateNoteFrame))
+        display_notes_btn = self.create_buttom("Показать текущие заметки", lambda: master.show_frame(DisplayNotesFrame))
+        update_note_btn = self.create_buttom("Обновить текущие заметки", lambda: master.show_frame(UpdateNoteFrame))
+        delete_note_btn = self.create_buttom("Удалить заметку", lambda: master.show_frame(DeleteNoteFrame))
+        search_notes_btn = self.create_buttom("Поиск заметок", lambda: master.show_frame(SearchNoteFrame))
+        create_test_notes = self.create_buttom("Создать тестовые заметки", command=self.create_notes, side="bottom")
 
     def create_notes(self):
         nt = list(generate_notes(10))
-        notes.extend(nt)
+        try:
+            for note in nt:
+                self._nm.add_note(note)
+        except:
+            pass
 
 
 class CreateNoteFrame(BaseFrame):
@@ -175,17 +177,15 @@ class CreateNoteFrame(BaseFrame):
             if msg.endswith("статус"):
                 for st in _statuses:
                     lang_bt = ttk.Radiobutton(self, text=st, value=st, variable=self.select_status)
-                    lang_bt.pack(pady=6)
+                    lang_bt.pack(pady=1)
             else:
-
                 entry = ttk.Entry(self)
-                entry.pack(pady=6)
+                entry.pack(pady=1)
                 entry.bind("<Return>", self.focus_next_widget)
 
                 self.entries.append(entry)
 
-        self.but = ttk.Button(self, text="Добавить заметку", command=self.add_note_to_notes, width=30)
-        self.but.pack()
+        self.but = self.create_buttom(text="Добавить заметку", command=self.add_note_to_notes, width=30, pady=6)
         self.but.bind("<Return>", self.add_note_to_notes)
 
     def add_note_to_notes(self, event=None):
@@ -195,29 +195,33 @@ class CreateNoteFrame(BaseFrame):
             lst.append(entry.get())
             entry.delete(0, tk.END)
 
+        name, title, content, data = lst
         status = self.get_status()
+        del lst
 
-        if not validate_name_title(lst[0], lst[1]):
-            messagebox.showerror("Ошибка", "Имя и заголовок не должны быть пустыми")
+        if not data.strip():
+            data = (date_now + timedelta(days=7)).strftime("%d-%m-%Y")
+
+        if not self.validate_note_data(name, title, data):
             return
 
-        if not lst[-1].strip():
-            lst[-1] = date_now + timedelta(days=7)
-        elif validate_date(lst[-1]):
-            lst[-1] = dt.strptime(lst[-1], "%d-%m-%Y")
-        else:
-            messagebox.showerror("Ошибка", "Неверный формат даты")
-            return
+        note = create_note(name, title, content, status, date_now, data)
 
-        note = create_note(lst[0], lst[1], lst[2], status, date_now, lst[-1])
-
-        if validate_note(notes, note):
-            # self._notes.append(note)
-            notes.append(note)
+        try:
+            self._nm.add_note(note)
             messagebox.showinfo("Выполнено", "Заметка успешно добавлена")
-        else:
+        except ValueError:
             messagebox.showerror("Ошибка", "Заметка стаким пользователем уже существует!")
             return
+
+    def validate_note_data(self, name, title, data):
+        if not validate_name_title(name, title):
+            messagebox.showerror("Ошибка", "Имя и заголовок не должны быть пустыми")
+            return False
+        if not validate_date(data):
+            messagebox.showerror("Ошибка", "Неверный формат даты")
+            return False
+        return True
 
 
     def get_status(self):
@@ -226,14 +230,11 @@ class CreateNoteFrame(BaseFrame):
 
     def focus_next_widget(self, event):
         """Переход к следующему виджету"""
-        current_widget = event.widget
-        index = self.entries.index(current_widget)  # Определяем текущий индекс
-        if index < len(self.entries) - 1:
+        widgets = self.entries + [self.but]
+        index = widgets.index(event.widget)  # Определяем текущий индекс
+        if index < len(self.entries):
             # Переход к следующему полю
-            self.entries[index + 1].focus_set()
-        else:
-            # Если это последнее поле, передаем фокус на кнопку
-            self.but.focus_set()
+            widgets[index + 1].focus_set()
 
 
 
@@ -243,7 +244,7 @@ class DisplayNotesFrame(BaseFrame):
         self._message = "Окно для отображения заметок"
         master.message_label.set(self._message)
 
-        self.create_tree(notes)
+        self.create_tree(self._nm.notes)
 
 
 
@@ -292,9 +293,9 @@ class SearchNoteFrame(BaseFrame):
     def click(self):
         keyword = self.entry.get().strip() if self.entry.get().strip() else None
         self.entry.delete(0, tk.END)
-        s = self.select()
+        status = self.select()
 
-        n = search_notes(notes, keyword=keyword, status=s)
+        n = self._nm.get_notes(keyword=keyword, status=status)
         self.create_tree(n)
 
 
