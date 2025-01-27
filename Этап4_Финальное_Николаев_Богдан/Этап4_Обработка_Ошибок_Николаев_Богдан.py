eab1bafb1930a1c57d0ed7129597ffb5ab9734e8
BogdNV <nikolaevbv92@gmail.com>
2025-01-27 21:48:29 +0300

    add function error_handling

--- Этап4_Финальное_Николаев_Богдан/Этап4_Обработка_Ошибок_Николаев_Богдан.py
+++ Этап4_Финальное_Николаев_Богдан/Этап4_Обработка_Ошибок_Николаев_Богдан.py
@@ -1,16 +1,29 @@
 from Этап4_Загрузка_Заметок_Николаев_Богдан import load_notes_from_file
+from Этап4_Сохранение_Заметок_Николаев_Богдан import save_notes_to_file
+from Этап4_Добавление_Данных_Николаев_Богдан import append_notes_to_file
+from Этап4_JSON_Формат_Николаев_Богдан import save_notes_json
 from display_notes_function import display_notes
 
 
 filename = "notes_07_01_2025.yaml"
 
+def error_handling(func, filename, notes=None):
+    try:
+        if func is load_notes_from_file:
+            notes = func(filename)
+            display_notes(notes)
+        elif func is save_notes_to_file:
+            func(notes, filename)
+        elif func is append_notes_to_file:
+            func(notes, filename)
+        elif func is save_notes_json:
+            func(notes, filename)
+    except UnicodeDecodeError:
+        print(f"Ошибка при чтении файла {filename}. Проверьте его содержимое")
+    except PermissionError:
+        print("Ошибка доступа")
+    except Exception as e:
+        print(f"Ошибка {e}")
 
-try:
-    notes = load_notes_from_file(filename)
-    display_notes(notes)
-except UnicodeDecodeError:
-    print(f"Ошибка при чтении файла {filename}. Проверьте его содержимое")
-except PermissionError:
-    print("Ошибка доступа")
-except Exception as e:
-    print(f"Ошибка {e}")
+if __name__ == '__main__':
+    error_handling(load_notes_from_file, "notes_10_01_2025.yaml")
