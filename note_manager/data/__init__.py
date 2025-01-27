c5664d82f98ae70de727e552d2b697b5151ccf92
BogdNV <nikolaevbv92@gmail.com>
2025-01-27 17:49:25 +0300

    add save_notes_yaml

--- note_manager/data/__init__.py
+++ note_manager/data/__init__.py
@@ -1,2 +1,4 @@
-from .file_handling import (save_notes_to_file, load_notes_from_file,
-                            append_notes_to_file, save_notes_json, load_notes_json)
+from .file_handling import (save_notes_to_file, load_notes_from_file, save_notes_yaml,
+                            append_notes_to_file, save_notes_json, load_notes_json, append_notes_to_json)
+
+
